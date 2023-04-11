c3466928b941f12c5409c5c53ccbcf54f0ee455c
unknown <Souleima Gara>
2023-04-11 10:44:49 +0200

    fixing bugs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -15,6 +15,8 @@ public class GameManager : MonoBehaviour
     public Text BitoinsCount_V1;
     // Reference to the Text component that displays the bitcoins numbers of V2
     public Text BitoinsCount_V2;
+    // Reference to the Text component that displays the bitcoins numbers of V3
+    public Text BitoinsCount_V3;
     // Reference to the pabel warning 
     public GameObject Warning_Panel;
     // Reference to the CashOut panel
@@ -27,6 +29,15 @@ public class GameManager : MonoBehaviour
     public Text SlideAmount;
     // Reference to the Animator component 
     public Animator animator;
+    // Reference to the GamerTag component 
+    public InputField GamerTag;
+    // Reference to the error text component 
+    public GameObject ErrorText ;
+    // Reference to the GamerTag component 
+    public GameObject SuccessPanel;
+    // Reference to the GamerTag component 
+    public GameObject WithdrawPanel;
+
 
 
     [Header("Images to change Color")]
@@ -60,18 +71,20 @@ public class GameManager : MonoBehaviour
     IEnumerator UpdateTextAfterAnimation()
     {
         animator.SetTrigger("PlayAnimation"); // replace "PlayAnimation" with the name of your animation trigger
-        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
-        // Increase the score by 10
+                                              // Increase the score by 10
         count = count + 10;
         // Update the score text
         BitoinsCount_V2.text = count.ToString() + " sats";
         //set animation controller to false
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+       
         isAnimationPlaying = false;
     }
     public void CollectBitcoins_V3()
     {
         // Increase the score by 10
         count = count + 10;
+        BitoinsCount_V3.text = count.ToString() + " sats";
     }
     public void CashOutBitcoin()
     {
@@ -101,7 +114,9 @@ public class GameManager : MonoBehaviour
         count = 0;
         BitoinsCount_V1.text = "0";
         BitoinsCount_V2.text = "0 sats";
+        BitoinsCount_V3.text = "0 sats";
         Slider.value = 0;
+        GamerTag.text = "";
     }
     //convert bitcoint amount to Slider
     public void FillSlider()
@@ -136,7 +151,20 @@ public class GameManager : MonoBehaviour
     }
 
 
-
+    public void checkGamerTagFieal()
+    {
+       if(  string.IsNullOrEmpty(GamerTag.text) )
+       {
+            ErrorText.SetActive(true);
+       }
+       else
+       {
+            ErrorText.SetActive(false);
+            WithdrawPanel.SetActive(false);
+            SuccessPanel.SetActive(true);
+            ClearAmount();
+        }
+    }
 
 
 }
