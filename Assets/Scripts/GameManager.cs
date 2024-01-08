aa07c63c2259ade675a7a006955ba043c01b2ad8
souleimagara <36776087+souleimagara@users.noreply.github.com>
2024-01-08 17:29:00 +0100

    Update UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
@@ -36,21 +38,24 @@ public class GameManager : MonoBehaviour
     // Reference to the Slider Amount
     public Text SlideAmount;
     // Reference to the GamerTag component 
-    public InputField GamerTagInputField;
+    public InputField EmailInputField;
     // Reference to converted sats
     public Text convertedsatsamounts;
-
+    // Reference to converted dollars
+    public Text Converteddollarsamounts;
     [Header("BitCoin Counter - Cashout - V2 - Elements")]
     // Reference to the Animator component 
     public Animator animator;
 
     [Header("CashOut-Bitcoin - V2 - Limit reached")]
     // Reference to the gamertag inputfield
-    public InputField gamtertaginputfield_limit;
+    public InputField Emailinputfield_limit;
     // Reference to the bitcoint text
     public Text bitcointext_limit;
     // Reference to the converted sats amount
     public Text satsamountconvertedlimit;
+    // Reference to the converted bitcount amount
+    public Text bitcoinamountconvertedlimit;
 
     [Header("Liste of Sprites to change Color")]
     // Reference to all images to change their color
@@ -67,16 +72,19 @@ public class GameManager : MonoBehaviour
     // Color to change
     private Color SelectedColor;
     // Controle Arrow Animation
-    private bool isAnimationPlaying = false;
+    private bool isAnimationPlaying;
     // The total stats amount
     private int Totalsatsamount = 0;
 
     private const int BITCOIN_LIMIT = 200;
     private const int BITCOIN_EARN_AMOUNT = 10;
+    private float satsamount;
+
+    private string bitcoinToDollarApiUrl = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd";
     #endregion
     public void CollectBitcoins_V1()
     {
-       
+
 
         if (BitcoinEarnedAmount >= BITCOIN_LIMIT)
         {
@@ -99,39 +107,39 @@ public class GameManager : MonoBehaviour
 
     public void CollectBitcoins_V2()
     {
-       if( BitcoinEarnedAmount < BITCOIN_LIMIT)
-       {
+        if (BitcoinEarnedAmount < BITCOIN_LIMIT)
+        {
             //start arrow animation
             StartCoroutine(UpdateTextAfterAnimation());
             isAnimationPlaying = true;
-       }
-       else
-       {
+        }
+        else
+        {
             // make all button non interactable
             foreach (Button btn in CashOutButtons)
             {
                 btn.interactable = false;
             }
         }
-          
-        
+
+
 
     }
     IEnumerator UpdateTextAfterAnimation()
     {
         animator.SetTrigger("PlayAnimation"); // replace "PlayAnimation" with the name of your animation trigger
-                                             
+
         BitcoinEarnedAmount = BitcoinEarnedAmount + BITCOIN_EARN_AMOUNT; // Increase the score by 10
         // Update the score text
         BitoinsCount_V2.text = BitcoinEarnedAmount.ToString() + " sats";
         //set animation controller to false
         yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
-       
+
         isAnimationPlaying = false;
     }
     public void CollectBitcoins_V3()
     {
-       
+
 
         if (BitcoinEarnedAmount >= BITCOIN_LIMIT)
         {
@@ -152,68 +160,69 @@ public class GameManager : MonoBehaviour
     }
     public void CashOutBitcoin()
     {
-       
-           // Check if the number of Bitcoin is greater than 0 to open the withdraw panel if not open the warning panel.
-            if (BitcoinEarnedAmount <= 0)
-            {
-               Warning_Panel.SetActive(true);
-               return;
-            }
-       
-            if(Totalsatsamount >= BITCOIN_LIMIT)
-            {
-                gamtertaginputfield_limit.text = GamerTagInputField.text;
-                CashOut_Limtit_Reached_Panel.SetActive(true);
-                bitcointext_limit.text = BitcoinEarnedAmount.ToString();
-                satsamountconvertedlimit.text = ConvertBitcoinToStats(BitcoinEarnedAmount);
-                return;
+        StartCoroutine(FetchExchangeRate());
+        // Check if the number of Bitcoin is greater than 0 to open the withdraw panel if not open the warning panel.
+        if (BitcoinEarnedAmount <= 0)
+        {
+            Warning_Panel.SetActive(true);
+            return;
+        }
 
-            }
-           
-                
-              
-                GetBitcoinAmount();
-                FillSlider();
-                calculatestatsamount();
-                CashOut_Panel.SetActive(true);
-            
-           
-       
+        if (Totalsatsamount >= BITCOIN_LIMIT)
+        {
+            Emailinputfield_limit.text = EmailInputField.text;
+            CashOut_Limtit_Reached_Panel.SetActive(true);
+            bitcointext_limit.text = BitcoinEarnedAmount.ToString();
+            satsamountconvertedlimit.text = ConvertBitcoinToStats(BitcoinEarnedAmount);
+            return;
+
+        }
+
+
+
+        GetBitcoinAmount();
+        FillSlider();
+        calculatestatsamount();
+        CashOut_Panel.SetActive(true);
+
+
+
     }
-  
+
     // get bitcoin amount to withdraw and check if it reached the limit of 200 or no
     public void GetBitcoinAmount()
     {
         if (BitcoinEarnedAmount + Totalsatsamount > BITCOIN_LIMIT)
         {
-             int rest = BITCOIN_LIMIT - Totalsatsamount;
-             BitcoinEarnedAmount -= rest;
-           
+            int rest = BITCOIN_LIMIT - Totalsatsamount;
+            BitcoinEarnedAmount -= rest;
+
             Totalsatsamount += rest;
-            BitcoinAmount.text = Totalsatsamount.ToString();
+            BitcoinAmount.text = BitcoinEarnedAmount.ToString();
             BitoinsCount_V1.text = BitcoinEarnedAmount.ToString();
             BitoinsCount_V2.text = BitcoinEarnedAmount.ToString();
             BitoinsCount_V3.text = BitcoinEarnedAmount.ToString();
-            bitcointext_limit.text = BitcoinEarnedAmount.ToString();    
-           
+            bitcointext_limit.text = BitcoinEarnedAmount.ToString();
+
             satsamountconvertedlimit.text = ConvertBitcoinToStats(BitcoinEarnedAmount);
         }
         else
         {
             Totalsatsamount += BitcoinEarnedAmount;
-            BitcoinAmount.text = Totalsatsamount.ToString();
+            BitcoinAmount.text = BitcoinEarnedAmount.ToString();
             BitcoinEarnedAmount = 0;
             BitoinsCount_V1.text = "0";
             BitoinsCount_V2.text = "0 sats";
             BitoinsCount_V3.text = "0 sats";
         }
 
-       
+
     }
 
     private string ConvertBitcoinToStats(float bitcoinAmount)
     {
         float statsAmount = bitcoinAmount / 10000000f;
+        satsamount = statsAmount;
         return "₿" + statsAmount.ToString("F6");
     }
     //convert bitcoint amount to Slider
@@ -228,6 +237,7 @@ public class GameManager : MonoBehaviour
         int bitcoinamount;
         int.TryParse(BitcoinAmount.text, out bitcoinamount);
         float statsamount = bitcoinamount / 10000000f;
+        satsamount = statsamount;
         string resultString = statsamount.ToString("F6");
         convertedsatsamounts.text = "₿" + resultString;
 
@@ -235,17 +245,17 @@ public class GameManager : MonoBehaviour
 
     }
 
-  
+
     //Open the link for download the zebedee wallet
     public void GetZebedeeWallet()
     {
 
-        Application.OpenURL("https://zebedee.io/app?partner=ZBD&game=Sarutobi&game_name=Sarutobi");
+        Application.OpenURL("https://apps.apple.com/us/app/zbd-games-rewards-bitcoin/id1484394401?mt=8");
     }
     // change color of all the selected sprites
     public void ChangeColor(Image colorselected)
     {
-        
+
         SelectedColor = colorselected.color;
         ApplyColorToOtherImages(SelectedColor);
         getsatsamount();
@@ -253,18 +263,18 @@ public class GameManager : MonoBehaviour
 
     void ApplyColorToOtherImages(Color selectedColor)
     {
-        
-       
+
+
         foreach (Image image in ColorofImagesToChange)
         {
-           
-                image.color = SelectedColor;
-           
+
+            image.color = SelectedColor;
+
         }
 
-        
+
     }
-    
+
     public void getsatsamount()
     {
         BitoinsCount_V1.text = BitcoinEarnedAmount.ToString();
@@ -274,12 +284,14 @@ public class GameManager : MonoBehaviour
     // check if gamtertag inputfield is empty or no
     public void checkGamerTagFieal()
     {
-       if(  string.IsNullOrEmpty(GamerTagInputField.text) )
-       {
+
+        string email = EmailInputField.text;
+        if (string.IsNullOrEmpty(EmailInputField.text) || !IsValidEmail(email))
+        {
             ErrorPanel.SetActive(true);
-       }
-       else
-       {
+        }
+        else
+        {
             ErrorPanel.SetActive(false);
             CashOut_Panel.SetActive(false);
             SuccessPanel.SetActive(true);
@@ -287,8 +299,69 @@ public class GameManager : MonoBehaviour
             {
                 btn.interactable = true;
             }
-       }
+        }
+    }
+
+    bool IsValidEmail(string email)
+    {
+        // This regex pattern is a simple one for basic email validation.
+        string pattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+        Regex regex = new Regex(pattern);
+        return regex.IsMatch(email);
     }
 
 
+
+    IEnumerator FetchExchangeRate()
+    {
+        UnityWebRequest request = UnityWebRequest.Get(bitcoinToDollarApiUrl);
+        yield return request.SendWebRequest();
+
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            string response = request.downloadHandler.text;
+
+            float exchangeRate = ParseExchangeRate(response);
+            float dollarsAmount = ConvertBitcoinToDollars(satsamount, exchangeRate);
+            Debug.Log("Bitcoin: " + satsamount + " BTC is equivalent to $" + dollarsAmount);
+            float roundedBitcoinAmount = Mathf.Round(dollarsAmount * 100) / 100; // Round to two decimal places
+            Converteddollarsamounts.text = "$ " + roundedBitcoinAmount.ToString();
+            bitcoinamountconvertedlimit.text = "$ " + roundedBitcoinAmount.ToString();
+
+        }
+        else
+        {
+            Debug.LogError("Failed to fetch exchange rate: " + request.error);
+            float exchangeRate = 50000f;
+            float dollarsAmount = ConvertBitcoinToDollars(satsamount, exchangeRate);
+            float roundedBitcoinAmount = Mathf.Round(dollarsAmount * 100) / 100; // Round to two decimal places
+            Converteddollarsamounts.text = "$ " + roundedBitcoinAmount.ToString();
+            bitcoinamountconvertedlimit.text = "$ " + roundedBitcoinAmount.ToString();
+
+        }
+    }
+
+    float ParseExchangeRate(string jsonResponse)
+    {
+        ExchangeRateData exchangeRateData = JsonUtility.FromJson<ExchangeRateData>(jsonResponse);
+        return exchangeRateData.bitcoin.usd;
+    }
+
+    float ConvertBitcoinToDollars(float bitcoinAmount, float exchangeRate)
+    {
+        return bitcoinAmount * exchangeRate;
+    }
+
+}
+
+[System.Serializable]
+public class ExchangeRateData
+{
+    public BitcoinData bitcoin;
+}
+
+[System.Serializable]
+public class BitcoinData
+{
+    public float usd;
 }
